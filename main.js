e4c29f633f1fd124c71cb45061e2ecd3f0f3bf71
turing-Linh <chaugialinh1202@gmail.com>
2026-05-20 13:56:16 +0700

    submit

---
+++ main.js
@@ -0,0 +1,11 @@
+let iscertificated = true;
+
+let average_score = 80;
+
+let dilligent = 60;
+
+if (iscertificated == true || (dilligent >= 50 && average_score >= 80))
+  console.log("sinh viên được dự thi");
+else {
+  console.log("sinh viên không được dự thi");
+}
